ff22a9263271fbbe7f8a75f4b656b9830d8cafa5
Adam Albright <aalbright425@gmail.com>
2017-04-20 16:03:55 -0400

    Tring to fix compile Error

---
+++ src/app/admin/view-appointments.component.ts
@@ -0,0 +1,33 @@
+import 'rxjs/add/operator/switchMap';
+import { Component, OnInit }      from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { Location }               from '@angular/common';
+import { Router } from '@angular/router';
+import { AuthService } from '../auth.service';
+
+import { Appointment }     from '../appointment/appointment';
+import { AppointmentService }   from '../appointment/appointment.service';
+
+@Component({
+  selector: 'view-appointments',
+  templateUrl: './view-appointments.component.html'
+})
+
+export class ViewAppointmentsComponent implements OnInit {
+  appointments: Appointment [];
+
+  constructor(
+    private appointmentService: AppointmentService,
+    private router: Router,
+    private route: ActivatedRoute,
+    private location: Location,
+    private authService: AuthService
+  ) {}
+
+  ngOnInit(): void {
+    let currentUser = JSON.parse(localStorage.getItem('currentUser'));
+    if (currentUser.accessLevel === 'Admin') {
+        this.appointmentService.getAppointmentsByBusinessId(currentUser.company_id).then(appointments => this.appointments = appointments);
+      }
+  }
+}
